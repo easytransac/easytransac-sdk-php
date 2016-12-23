517e29d1d2d79f26dc78767d33b213e8341ef0ee
Klyde THEME <klyde@movidone.com>
2016-12-23 12:48:02 +0100

    Fix names collision in the sample

--- samples/status.php
+++ samples/status.php
@@ -4,7 +4,7 @@ require_once(__DIR__.'/../sdk/EasyTransac/autoload.php');
 
 use EasyTransac\Core\Services;
 use EasyTransac\Entities\PaymentStatus;
-use EasyTransac\Requests\PaymentStatus;
+use EasyTransac\Requests;
 
 Services::getInstance()->setDebug(true);
 Services::getInstance()->provideAPIKey('a1b2c3d4');
@@ -12,7 +12,7 @@ Services::getInstance()->provideAPIKey('a1b2c3d4');
 $transaction = new PaymentStatus();
 $transaction->setTid('a1b2c3d4');
 
-$request = new PaymentStatus();
+$request = new Requests\PaymentStatus();
 $response = $request->execute($transaction);
 
 if ($response->isSuccess())
