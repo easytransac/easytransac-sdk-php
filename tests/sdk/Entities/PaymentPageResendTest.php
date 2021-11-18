8ddab216fc3112759285c34041711c94c39d85c0
Damien <damien@movidone.com>
2021-11-18 11:00:09 +0100

    Add payment page resend feature

---
+++ tests/sdk/Entities/PaymentPageResendTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use EasyTransac\Entities\PaymentPageResend;
+use PHPUnit\Framework\TestCase;
+
+class PaymentPageResendTest extends TestCase
+{
+    public function testHydrate()
+    {
+        $c = new PaymentPageResend();
+        $c->hydrate(json_decode(json_encode($this->getFixture())));
+
+        var_dump($c->toArray());
+        $this->assertEquals($c->toArray(), $this->getFixture());
+    }
+
+    protected function getFixture(): array
+    {
+        return [
+            "RequestId" => "111",
+            "Language" => "FRE",
+        ];
+    }
+}
