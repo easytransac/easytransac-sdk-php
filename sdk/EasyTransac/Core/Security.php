1db7980d00b03af61a2c26d09a983d66a7b81b14
Damien <damien@movidone.com>
2021-11-22 10:46:41 +0100

    Add comments

--- sdk/EasyTransac/Core/Security.php
+++ sdk/EasyTransac/Core/Security.php
@@ -10,7 +10,7 @@ class Security
 {
     /**
      * Returns the signature from a params list and api key for a request
-     * @param Array $params
+     * @param array $params
      * @param String $apiKey
      * @return String
      */
@@ -22,23 +22,32 @@ class Security
 
         $signature = '';
         if (is_array($params)) {
+            // set keys to lower case
             $params = array_change_key_case($params, CASE_LOWER);
+            // order by alpha.
             ksort($params);
 
             foreach ($params as $name => $valeur) {
+                // if object, cast as array, we are in node
                 if (is_object($valeur)) {
                     $valeur = (array) $valeur;
                 }
 
+                // if key is not "Signature"
                 if (strcasecmp($name, 'signature') != 0) {
+                    // if object
                     if (is_array($valeur)) {
+                        // set values to lower case
                         $valeur = array_change_key_case($valeur, CASE_LOWER);
+                        // order by alpha.
                         ksort($valeur);
 
+                        // parse node values and assign value
                         foreach ($valeur as $v) {
                             $signature .= $v . '$';
                         }
                     } else {
+                        // if it's a value
                         $signature .= $valeur . '$';
                     }
                 }
